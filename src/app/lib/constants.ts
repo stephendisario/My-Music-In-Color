bb0e07e5e5e7b292bb1279bad19f8e4d0cbfb712
Stephen Disario <stephendisario2@gmail.com>
2024-05-22 13:14:20 -0400

    env (#2)

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -1,6 +1,5 @@
 export const clientId = "53e53a540f804d048a8f6fc2c653ee0e";
-export const redirectUrl = "http://localhost:3000/callback";
-
+export const redirectUrl = process.env.NEXT_PUBLIC_REDIRECT_URI || "http://localhost:3000/callback";
 export const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 export const tokenEndpoint = "https://accounts.spotify.com/api/token";
 export const scope =
