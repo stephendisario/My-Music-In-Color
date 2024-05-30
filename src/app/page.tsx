09f1975477548e6fe9ba915cbeff68f1464ae2c4
stephendisario <sdisario@umass.edu>
2024-05-30 12:43:48 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 import { redirect } from "next/navigation";
-import { getTopTracks, getUserProfile } from "./api/spotify";
 
 const Home = async () => {
-  const user = await getUserProfile();
-  const loadingTracks = await getTopTracks("short_term", 1);
-  const topTracks = await getTopTracks("long_term");
-
-  return (
-    <div>hey {user?.display_name}</div>
-  )
-
+  redirect("/dashboard");
 };
 
 export default Home;
