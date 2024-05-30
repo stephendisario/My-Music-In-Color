9aed30239ed2e3b03416e65a1249d999092041dd
stephendisario <sdisario@umass.edu>
2024-05-30 14:09:55 -0400

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,28 @@
-import type { NextRequest } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get('session')?.value
- 
-  if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
-    return Response.redirect(new URL('/dashboard', request.url))
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { decrypt } from "./app/lib/session";
+
+const protectedRoutes = ["/", "/dashboard"];
+const publicRoutes = ["/login", "/callback"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const cookie = cookies().get("session")?.value;
+
+  if (isProtectedRoute && !cookie) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
- 
-  if (!currentUser && !request.nextUrl.pathname.startsWith('/login') || !request.nextUrl.pathname.startsWith('/callback')) {
-    return Response.redirect(new URL('/login', request.url))
+
+  if (isPublicRoute && cookie) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
+
+  return NextResponse.next();
 }
- 
+
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-}
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
