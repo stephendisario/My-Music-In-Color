20c358698479a759b35bfcd20ceb37f157017d1e
stephendisario <sdisario@umass.edu>
2024-05-30 14:04:20 -0400

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,17 @@
-import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { decrypt } from "./app/lib/session";
-
-const protectedRoutes = ["/", "/dashboard"];
-const publicRoutes = ["/login", "/callback"];
-
-export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
-
-  const cookie = cookies().get("session")?.value;
-  const session = await decrypt(cookie);
-
-  if (isProtectedRoute && !session?.payload) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get('session')?.value
+ 
+  if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
+    return Response.redirect(new URL('/dashboard', request.url))
   }
-
-  if (isPublicRoute && session?.payload) {
-    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+ 
+  if (!currentUser && !request.nextUrl.pathname.startsWith('/login')) {
+    return Response.redirect(new URL('/login', request.url))
   }
-
-  return NextResponse.next();
 }
-
+ 
 export const config = {
-  matcher: '/((?!_next/static|_next/image).*)',
-};
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
