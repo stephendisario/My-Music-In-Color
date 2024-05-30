39b21e8a5baa9a79c135a889764882c220425dc9
stephendisario <sdisario@umass.edu>
2024-05-30 13:40:57 -0400

    .

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { decrypt } from "./app/lib/session";
+
+const protectedRoutes = ["/", "/dashboard"];
+const publicRoutes = ["/login", "/callback"];
+
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const cookie = cookies().get("session")?.value;
+  const session = await decrypt(cookie);
+
+  if (isProtectedRoute && !session?.payload) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+
+  if (isPublicRoute && session?.payload) {
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
