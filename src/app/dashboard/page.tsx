1b483732099f43c2c3f5c433ae6e256309c455f5
Stephen Disario <stephendisario2@gmail.com>
2024-05-30 01:19:07 -0400

    Develop (#11)
    * .
    
    * .
    
    * fix plz
    
    * .
    
    * . 2

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,23 +3,11 @@ import { getTopTracks, getUserProfile } from "../api/spotify";
 import ContentWrapper from "./ContentWrapper";
 import Welcome from "./Welcome";
 import { MyContextProvider } from "../components/ColorContext";
-import { cookies } from "next/headers";
-import { decrypt } from "../lib/session";
-import { redirect } from "next/navigation";
-// export const dynamic = "force-dynamic";
 
 const Dashboard = async () => {
-
-  const cookie = cookies().get("session")?.value;
-  const session = await decrypt(cookie);
-
-  if (!session?.payload) {
-    redirect("/login");
-  }
-
-  const user = await getUserProfile(session.payload);
-  const loadingTracks = await getTopTracks("short_term", 1, session.payload);
-  const topTracks = await getTopTracks("long_term", 0, session.payload);
+  const user = await getUserProfile();
+  const loadingTracks = await getTopTracks("short_term", 1);
+  const topTracks = await getTopTracks("long_term");
 
   return (
     <MyContextProvider initialValue={topTracks!}>
