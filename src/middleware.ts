56ed53750c9f19c985f2baf76c70503885cd09c6
stephendisario <sdisario@umass.edu>
2024-05-30 12:54:45 -0400

    does this break it

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,3 +23,8 @@ export default async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
+// Routes Middleware should not run on
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  };
