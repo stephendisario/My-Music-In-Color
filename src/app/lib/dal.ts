aa54013d85078e7f3b282f338aa33bd0ed48ceca
Stephen Disario <stephendisario2@gmail.com>
2024-05-30 00:44:45 -0400

    Update dal.ts

--- src/app/lib/dal.ts
+++ src/app/lib/dal.ts
@@ -5,12 +5,20 @@ import { decrypt } from "@/app/lib/session";
 import { redirect } from "next/navigation";
 
 export const verifySession = cache(async () => {
-  const cookie = cookies().get("session")?.value;
-  const session = await decrypt(cookie);
+  try {
+    const cookie = cookies().get("session")?.value;
+    if (!cookie) {
+      redirect("/login");
+    }
 
-  if (!session?.payload) {
+    const session = await decrypt(cookie);
+    if (!session?.payload) {
+      redirect("/login");
+    }
+
+    return { payload: session.payload };
+  } catch (error) {
+    console.error("Error verifying session:", error);
     redirect("/login");
   }
-
-  return { payload: session?.payload };
 });
