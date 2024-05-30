92bc2c09a0b2d3a9ec291fe2d541903689b15348
stephendisario <sdisario@umass.edu>
2024-05-30 11:59:11 -0400

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,30 @@
-import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { decrypt } from "./app/lib/session";
+// import { NextRequest, NextResponse } from "next/server";
+// import { cookies } from "next/headers";
+// import { decrypt } from "./app/lib/session";
 
-const protectedRoutes = ["/", "/collage", "/picker", "/playlist", "/dashboard"];
-const publicRoutes = ["/login", "/callback"];
+// const protectedRoutes = ["/", "/collage", "/picker", "/playlist", "/dashboard"];
+// const publicRoutes = ["/login", "/callback"];
 
-export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
+// export default async function middleware(req: NextRequest) {
+//   const path = req.nextUrl.pathname;
+//   const isProtectedRoute = protectedRoutes.includes(path);
+//   const isPublicRoute = publicRoutes.includes(path);
 
-  const cookie = cookies().get("session")?.value;
-  const session = await decrypt(cookie);
+//   const cookie = cookies().get("session")?.value;
+//   const session = await decrypt(cookie);
 
-  if (isProtectedRoute && !session?.payload) {
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
-  }
+//   if (isProtectedRoute && !session?.payload) {
+//     return NextResponse.redirect(new URL("/login", req.nextUrl));
+//   }
 
-  if (isPublicRoute && session?.payload && !req.nextUrl.pathname.endsWith("/")) {
-    return NextResponse.redirect(new URL("/", req.nextUrl));
-  }
+//   if (isPublicRoute && session?.payload && !req.nextUrl.pathname.endsWith("/")) {
+//     return NextResponse.redirect(new URL("/", req.nextUrl));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-// Routes Middleware should not run on
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
-};
+// // Routes Middleware should not run on
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+// };
