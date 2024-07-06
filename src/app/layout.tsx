0c7de9da46776b14f5108693d9f46cd5b3a19dde
Stephen Disario <stephendisario2@gmail.com>
2024-07-06 00:41:05 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,6 @@ import { MyContextProvider } from "./components/ColorContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const revalidate = 86400
-export const fetchCache = "force-cache";
-
 // export const metadata: Metadata = {
 //   title: "My Music In Color",
 //   // description: "Visualize ",
